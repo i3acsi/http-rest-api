1c7f56f221eb7f82bfd0b3a235bf322b8792cfae
i3acsi <gasevskyv@gmail.com>
2021-04-26 22:26:05 +0700

    user validation with tests

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -6,6 +6,82 @@ import (
 	"testing"
 )
 
+// TestUser_Validate ...
+func TestUser_Validate(t *testing.T) {
+	testCases := []struct {
+		name    string
+		u       func() *model.User
+		isValid bool
+	}{
+		{
+			name: "valid",
+			u: func() *model.User {
+				return model.TestUser(t)
+			},
+			isValid: true,
+		},
+		{
+			name: "valid with encrypted password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.EncryptedPassword = "encryptedpassword"
+				u.Password = ""
+				return u
+			},
+			isValid: true,
+		},
+		{
+			name: "empty email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "invalid email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = "email.ru"
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "empty password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "short password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = "12345"
+				return u
+			},
+			isValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.u().Validate())
+			} else {
+				assert.Error(t, tc.u().Validate())
+			}
+		})
+	}
+
+	u := model.TestUser(t)
+	assert.NoError(t, u.Validate())
+}
+
 // TestUser_BeforeCreate ...
 func TestUser_BeforeCreate(t *testing.T) {
 	u := model.TestUser(t)
--- internal/app/store/userRepo.go
+++ internal/app/store/userRepo.go
@@ -11,6 +11,9 @@ type UserRepo struct {
 
 // Create ...
 func (r *UserRepo) Create(u *model.User) (*model.User, error) {
+	if err := u.Validate(); err != nil {
+		return nil, err
+	}
 	if err := u.BeforeCreate(); err != nil {
 		return nil, err
 	}
